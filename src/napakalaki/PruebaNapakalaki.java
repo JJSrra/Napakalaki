48c9948339f3b2d6b4bb7d0e86a809875b09b4c2
usuario <usuario@172.20.50.27>
2015-10-16 12:11:18 +0200

    Añadidos los 3 primeros monstruos de la página 2

--- src/napakalaki/PruebaNapakalaki.java
+++ src/napakalaki/PruebaNapakalaki.java
@@ -26,6 +26,23 @@ public class PruebaNapakalaki {
 				new ArrayList(Arrays.asList()));
 		Prize pBicefalo = new Prize(1, 1);
 		monsters.add(new Monster("Bicéfalo", 20, bcBicefalo, pBicefalo));
+		
+		// Los hondos
+		BadConsequence bcHondos = new BadConsequence("Estos monstruos resultan bastante superficiales y te aburren mortalmente. Estás muerto.", true);
+		Prize pHondos = new Prize(2,1);
+		monsters.add(new Monster("Los hondos", 8, bcHondos, pHondos));
+		
+		// Semillas Cthulhu
+		BadConsequence bcSemillas = new BadConsequence("Pierdes 2 niveles y 2 tesoros ocultos.", 2, 0, 2);
+		Prize pSemillas = new Prize(2,1);
+		monsters.add(new Monster("Semillas Cthulhu", 4, bcSemillas, pSemillas));
+		
+		// Dameargo
+		BadConsequence bcDameargo = new BadConsequence("Te intentas escaquear. Pierdes una mano visible.", 0,
+				new ArrayList(Arrays.asList(TreasureKind.ONEHAND)),
+				new ArrayList(Arrays.asList()));
+		Prize pDameargo = new Prize(2,1);
+		monsters.add(new Monster("Dameargo", 1, bcDameargo, pDameargo));
 	}
 
 }
