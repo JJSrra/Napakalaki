ec2c0c57c553e1ca42bd647c0e6d5fe721f96a8c
diego <diegodan1893@gmail.com>
2015-12-11 12:29:39 +0100

    Añadida la nueva y revolucionaria clase BadConsequence!! Ahora con más abstracción.

---
+++ src/NapakalakiGame/BadConsequence.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NapakalakiGame;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author diego
+ */
+public abstract class BadConsequence {
+	
+	static final int MAXTREASURES = 10;
+	private String text;
+	private int levels;
+	
+	public String getText(){
+		return text;
+	}
+	
+	public int getLevels(){
+		return levels;
+	}
+	
+	public abstract boolean isEmpty();
+	public abstract void substractVisibleTreasure(Treasure t);
+	public abstract void substractHiddenTreasure(Treasure t);
+	public abstract BadConsequence adjustToFitTreasureLists(ArrayList<Treasure> v, ArrayList<Treasure> h);
+}
